d61e03f7ff350829be3eaba4699625f09946427c
Fabio Silveira <fabio.eds@hotmail.com>
2025-06-26 23:42:57 -0700

    AddedCode

--- sleeper-nfl-app/src/app/page.tsx
+++ sleeper-nfl-app/src/app/page.tsx
@@ -13,13 +13,14 @@ type Player = {
 export default function Home() {
 
   const [player, setPlayer] = useState<Player[]>([]);
+  const [searchPlayer, setSearchPlayer] = useState("")
 
   useEffect(() => {
     async function fetchApi() {
       try {
         const req = await axios.get("https://api.sleeper.app/v1/players/nfl")
-        const getPlayers = Object.values(req.data) as Player []
-        const fifteenPlayers = getPlayers.slice(0,15)
+        const getPlayers = Object.values(req.data) as Player[]
+        const fifteenPlayers = getPlayers.slice(0, 15)
         setPlayer(fifteenPlayers)
       } catch (error) {
         console.error("error to load api", error)
@@ -28,24 +29,35 @@ export default function Home() {
     fetchApi();
   }, [])
 
+  
+    const filteredPlayers = player.filter((e) => `${e.full_name}`.toLowerCase().includes(searchPlayer.toLowerCase()))
+
+
   return (
     <>
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Position</th>
-        </tr>
-      </thead>
-      <tbody>
-        {player.map((e) => (
-          <tr key={e.id_player}>
-            <td>{e.full_name}</td>
-            <td>{e.position}</td>
+
+      <input
+        type="text"
+        placeholder="enter a player"
+        value={searchPlayer}
+        onChange={(event) => setSearchPlayer(event.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredPlayers.map((e) => (
+            <tr key={e.id_player}>
+              <td>{e.full_name}</td>
+              <td>{e.position}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
